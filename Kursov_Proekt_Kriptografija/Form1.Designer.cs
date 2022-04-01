d5a990ee568c07da0a7a87d23b86287ce1eb3a4b
Matej Kostov <matej4o9@gmail.com>
2022-04-01 20:31:13 +0300

    Add project files.

---
+++ Kursov_Proekt_Kriptografija/Form1.Designer.cs
@@ -0,0 +1,388 @@
+﻿
+namespace Kursov_Proekt_Kriptografija
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button8 = new System.Windows.Forms.Button();
+            this.label11 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button9 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Location = new System.Drawing.Point(201, 380);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(105, 29);
+            this.button3.TabIndex = 19;
+            this.button3.Text = "Clear All";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 363);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(177, 17);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Text Code after Decryption";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox3.Location = new System.Drawing.Point(12, 383);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(173, 22);
+            this.textBox3.TabIndex = 17;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(201, 323);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(106, 29);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "Decrypt";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.DecryptButton_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Location = new System.Drawing.Point(12, 326);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(173, 22);
+            this.textBox2.TabIndex = 15;
+            this.textBox2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox2_KeyDown);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 306);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(140, 17);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Encrypted Text Code";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 250);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(164, 17);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Text Code for Encryption";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(202, 266);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 30);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Encrypt";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.EncryptButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(12, 270);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(173, 22);
+            this.textBox1.TabIndex = 11;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(195, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(302, 38);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "RSA ALGORITHM";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 66);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(163, 17);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "Enter a prime number \'p\'";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox4.Location = new System.Drawing.Point(12, 86);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(535, 22);
+            this.textBox4.TabIndex = 21;
+            this.textBox4.Text = "313";
+            this.textBox4.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox4_KeyDown);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(9, 121);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(163, 17);
+            this.label6.TabIndex = 22;
+            this.label6.Text = "Enter a prime number \'q\'";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox5.Location = new System.Drawing.Point(12, 141);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(535, 22);
+            this.textBox5.TabIndex = 23;
+            this.textBox5.Text = "653";
+            this.textBox5.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox5_KeyDown);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 176);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(204, 17);
+            this.label7.TabIndex = 24;
+            this.label7.Text = "Calculate Phi(p*q) = (p-1)*(q-1)";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox6.Location = new System.Drawing.Point(12, 196);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(535, 22);
+            this.textBox6.TabIndex = 25;
+            this.textBox6.Text = "203424";
+            this.textBox6.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox6_KeyDown);
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button4.Location = new System.Drawing.Point(566, 186);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(105, 42);
+            this.button4.TabIndex = 27;
+            this.button4.Text = "Calculate Phi";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.CalculatePhiButton_Click);
+            // 
+            // button5
+            // 
+            this.button5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button5.Location = new System.Drawing.Point(566, 131);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(106, 42);
+            this.button5.TabIndex = 28;
+            this.button5.Text = "Check Number";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.PrimeButton2_Click);
+            // 
+            // button6
+            // 
+            this.button6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button6.Location = new System.Drawing.Point(566, 76);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(106, 42);
+            this.button6.TabIndex = 29;
+            this.button6.Text = "Check Number";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.PrimeButton1_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(369, 299);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(0, 17);
+            this.label9.TabIndex = 32;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(369, 250);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(222, 17);
+            this.label10.TabIndex = 34;
+            this.label10.Text = "Choose number for Encryption \'e\':";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(593, 250);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(76, 52);
+            this.listBox1.TabIndex = 37;
+            // 
+            // button8
+            // 
+            this.button8.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button8.Location = new System.Drawing.Point(429, 270);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(106, 31);
+            this.button8.TabIndex = 38;
+            this.button8.Text = "Generate \'e\'";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.GenerateButton_Click);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(364, 335);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(223, 17);
+            this.label11.TabIndex = 39;
+            this.label11.Text = "Choose number for Decryption \'d\':";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(593, 335);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(76, 52);
+            this.listBox2.TabIndex = 40;
+            // 
+            // button9
+            // 
+            this.button9.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button9.Location = new System.Drawing.Point(429, 355);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(106, 31);
+            this.button9.TabIndex = 41;
+            this.button9.Text = "Generate \'d\'";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.Generate2Button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 423);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button9;
+    }
+}
+
---
+++ Kursov_Proekt_Kriptografija/Form1.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Numerics;
+
+namespace Kursov_Proekt_Kriptografija
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region Help Functions
+
+        private static bool IsNumber(string s)
+        {
+            foreach (char c in s)
+            {
+                if (!char.IsDigit(c) && c != ' ')
+                    return false;
+            }
+            return true;
+        }
+
+        private static int Check_Prime(string s)
+        {
+            if (IsNumber(s) == false)
+                return 0;
+            else 
+            {
+            BigInteger number = Convert.ToInt64(s);
+            if (number == 1 || number == 0 || number < 0)
+                return 0;
+
+            for (int i = 2; i < number; i++)
+            {
+                if (number % i == 0)
+                    return 0;
+            }
+            return 1;
+            }
+        }
+
+        private static BigInteger NZD(BigInteger a, BigInteger b)
+        {
+            BigInteger rem;
+
+            while (b > 0)
+            {
+                rem = a % b;
+                a = b;
+                b = rem;
+            }
+            return a;
+        }
+
+        #endregion
+
+        #region ButtonClick Functions
+
+        private void PrimeButton1_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(textBox4.Text))
+                MessageBox.Show("Please Enter a Number");
+
+            else if (textBox5.Text == textBox4.Text)
+            {
+                MessageBox.Show("Please Enter Different Numbers");
+                textBox4.BackColor = Color.Pink;
+            }
+
+            else
+            {
+                string s = textBox4.Text;
+                if (Check_Prime(s) == 0)
+                    textBox4.BackColor = Color.Pink;
+                else
+                    textBox4.BackColor = Color.LightGreen;
+            }
+        }
+
+        private void PrimeButton2_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(textBox5.Text))
+                MessageBox.Show("Please Enter a Number");
+
+            else if (textBox5.Text == textBox4.Text)
+            {
+                MessageBox.Show("Please Enter Different Numbers");
+                textBox5.BackColor = Color.Pink;
+            }
+
+            else
+            {
+                string s = textBox5.Text;
+                if (Check_Prime(s) == 0)
+                    textBox5.BackColor = Color.Pink;
+                else
+                    textBox5.BackColor = Color.LightGreen;
+            }
+        }
+
+        private void CalculatePhiButton_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(textBox5.Text) || String.IsNullOrEmpty(textBox4.Text) ||
+                textBox5.BackColor == Color.Pink || textBox5.BackColor == Color.Pink)
+                MessageBox.Show("Please Enter Valid Prime Numbers!");
+
+            else
+            {
+                BigInteger a, b, c;
+                a = Convert.ToInt64(textBox4.Text);
+                b = Convert.ToInt64(textBox5.Text);
+                c = (a - 1) * (b - 1);
+
+                textBox6.Text = Convert.ToString(c);
+                textBox6.BackColor = Color.LightGreen;
+            }
+        }
+
+        private void GenerateButton_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            if (String.IsNullOrEmpty(textBox6.Text))
+                MessageBox.Show("Please Calculate Phi First!");
+            else
+            {
+                BigInteger a;
+                a = Convert.ToInt64(textBox6.Text);
+                
+                for (BigInteger i = 2; i < a; i++)
+                {
+                    if (NZD(i, a) == 1)
+                        listBox1.Items.Add(i);
+                }
+            }
+        }
+
+        private void Generate2Button_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Clear();
+            if (String.IsNullOrEmpty(listBox1.Text) || String.IsNullOrEmpty(listBox1.SelectedItem.ToString()))
+                MessageBox.Show("Please Choose a value for 'e'!");
+            else
+            {
+                BigInteger a = Convert.ToInt64(listBox1.SelectedItem.ToString());
+                BigInteger b = Convert.ToInt64(textBox6.Text);
+
+                for (BigInteger i = 2; i < 9999; i++)
+                {
+                    if (((i*a) % b) == 1)
+                        listBox2.Items.Add(i);
+                }
+            }
+        }
+
+        private void EncryptButton_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(listBox2.Text) || String.IsNullOrEmpty(listBox2.SelectedItem.ToString()) ||
+                String.IsNullOrEmpty(textBox6.Text) || String.IsNullOrEmpty(textBox1.Text))
+                MessageBox.Show("Please Choose a value for 'd' and Enter Text Code for Encryption");
+            else
+            {
+                int a = Convert.ToInt32(listBox1.SelectedItem.ToString());
+                BigInteger p = Convert.ToInt64(textBox4.Text);
+                BigInteger q = Convert.ToInt64(textBox5.Text);
+                BigInteger str = Convert.ToInt64(textBox1.Text);
+
+                BigInteger c = BigInteger.Pow(str, a);
+                textBox2.Text = Convert.ToString(c % (p * q));
+            }
+        }
+
+        private void DecryptButton_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(textBox2.Text) || String.IsNullOrEmpty(textBox1.Text))
+                MessageBox.Show("Please Enter Text Code and Encrypt");
+            else
+            {
+                BigInteger p = Convert.ToInt64(textBox4.Text);
+                BigInteger q = Convert.ToInt64(textBox5.Text);
+                BigInteger c = Convert.ToInt64(textBox2.Text);
+                int d = Convert.ToInt32(listBox2.SelectedItem.ToString());
+
+                BigInteger m = BigInteger.Pow(c, d);
+                BigInteger k = m % (p * q);
+                textBox3.Text = Convert.ToString(k);
+
+                if (textBox3.Text == textBox1.Text)
+                    textBox3.BackColor = Color.LightGreen;
+            }
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox3.BackColor = Color.White;
+            textBox4.Clear();
+            textBox4.BackColor = Color.White;
+            textBox5.Clear();
+            textBox5.BackColor = Color.White;
+            textBox6.Clear();
+            textBox6.BackColor = Color.White;
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+        }
+        #endregion
+
+        #region KeyPress Functions
+
+        private void textBox4_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            PrimeButton1_Click(sender, e);
+        }
+
+        private void textBox5_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                PrimeButton2_Click(sender, e);
+        }
+
+        private void textBox6_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                CalculatePhiButton_Click(sender, e);
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                EncryptButton_Click(sender, e);
+        }
+
+        private void textBox2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                DecryptButton_Click(sender, e);
+        }
+
+        #endregion
+    }
+}
